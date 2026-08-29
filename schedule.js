4fbf740eaef87007037c8807b9d74abcee090b0a
tamanitamayoga-svg <tamanitamayoga@gmail.com>
2026-08-29 20:27:57 +0900

    Add September 2026 open schedule

--- schedule.js
+++ schedule.js
@@ -23,6 +23,14 @@ const publishedSchedules = [
     // Edit August open days here when changes are needed.
     openDays: [1, 3, 4, 5, 6, 8, 9, 11, 16, 17, 18, 19, 20, 22, 25, 26, 28, 30, 31],
   },
+  {
+    year: 2026,
+    month: 9,
+    monthName: "\u9577\u6708",
+    openHours: "OPEN\u6642\u9593 08:00-20:00\uff08\u6c34\u306e\u307f 08:00-18:00\uff09",
+    // Edit September open days here when changes are needed.
+    openDays: [2, 3, 4, 5, 7, 8, 9, 15, 16, 17, 21, 23, 25, 27, 29, 30],
+  },
 ];
 
 const monthNames = [
